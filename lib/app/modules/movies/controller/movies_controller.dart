aeaf28de9450ef937ca0ee610c247bed1ffc0e61
Davi Almeida Matta da Silva <davialmeidamatta@gmail.com>
2026-02-25 13:58:01 -0300

    feat: criação de função para pegar  uma imagem

--- lib/app/modules/movies/controller/movies_controller.dart
+++ lib/app/modules/movies/controller/movies_controller.dart
@@ -1,10 +1,15 @@
 import 'package:biblioteca_flutter/app/routes/app_routes.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MoviesController extends GetxController {
   MoviesController();
 
-  void addNewMovie() {
+  final ImagePicker picker = ImagePicker();
+  final ValueNotifier<XFile?> imageNotifier = ValueNotifier<XFile?>(null);
+
+  addNewMovie() {
     Get.toNamed(Routes.MOVIE_FORM);
   }
 
@@ -15,6 +20,18 @@ class MoviesController extends GetxController {
     'synopsis': '',
     'image': '',
   };
-
+  
   get movieForm => null;
+
+  Future<void> pickImage() async {
+  try {
+    final XFile? picked = await picker.pickImage(source: ImageSource.gallery);
+    if (picked != null) {
+      imageNotifier.value = picked;
+      movieForm['image'] = picked.path;
+    }
+  } catch (e) {
+    print('Erro ao selecionar imagem: $e');
+  }
+  }
 }
