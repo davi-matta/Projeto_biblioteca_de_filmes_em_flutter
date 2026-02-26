974a8126681e4f02d73257fc933cfd824197ad7b
Davi Almeida Matta da Silva <davialmeidamatta@gmail.com>
2026-02-26 12:29:34 -0300

    fix: adição de bibiotecas para corrigir o firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:biblioteca_flutter/app/routes/app_pages.dart';
 import 'package:biblioteca_flutter/app/routes/app_routes.dart';
+import 'package:biblioteca_flutter/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+); 
   runApp(
   GetMaterialApp(
     initialRoute: Routes.SPLASH, getPages: AppPages.routes,
