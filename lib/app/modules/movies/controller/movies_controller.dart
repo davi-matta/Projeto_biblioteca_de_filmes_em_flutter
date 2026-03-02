cdb3ef362669cb32809fcde2cdf346b35e7b3ed6
Davi Almeida Matta da Silva <davialmeidamatta@gmail.com>
2026-03-02 14:36:27 -0300

    feat(movies_controller): adicionando _repository para gravação de dados

--- lib/app/modules/movies/controller/movies_controller.dart
+++ lib/app/modules/movies/controller/movies_controller.dart
@@ -1,14 +1,24 @@
+import 'package:biblioteca_flutter/app/modules/movies/model/movies_model.dart';
+import 'package:biblioteca_flutter/app/modules/movies/repository/movie_repository.dart';
+import 'package:biblioteca_flutter/app/modules/movies/service/image_service.dart';
 import 'package:biblioteca_flutter/app/routes/app_routes.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
 class MoviesController extends GetxController {
   MoviesController();
 
+  final MovieRepository _repository = MovieRepository();
+  
+
+  
+  
+  
   final ImagePicker picker = ImagePicker();
   final ValueNotifier<XFile?> imageNotifier = ValueNotifier<XFile?>(null);
-
+  @override
   addNewMovie() {
     Get.toNamed(Routes.MOVIE_FORM);
   }
