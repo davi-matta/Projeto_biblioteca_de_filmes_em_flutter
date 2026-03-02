2df8e1b69c4fdcefd31a44ded80bd5e8887432a0
Davi Almeida Matta da Silva <davialmeidamatta@gmail.com>
2026-03-02 13:15:52 -0300

    fix: mudança no código para a ddefinição da classe movie

--- lib/app/modules/movies/model/movies_model.dart
+++ lib/app/modules/movies/model/movies_model.dart
@@ -1,7 +1,18 @@
+class Movie {
+  final String? id;
+  final String title;
+  final String description;
+  final String directors;
+  final String synopsis;
+  final String? imageBase64;
 
+  Movie({
+    this.id,
+    required this.title,
+    required this.description,
+    required this.directors,
+    required this.synopsis,
+    this.imageBase64,
+  });
 
-
-
-class MovieProvider {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final String _collectionName = 'movies';
+  
