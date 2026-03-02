e9d7135e3eb82d56f0d44360965967eb96053f39
Davi Almeida Matta da Silva <davialmeidamatta@gmail.com>
2026-03-02 15:12:31 -0300

    feat: adicionando formas de pegar os filmes e colocar na lista, assim como imagens

--- lib/app/modules/movies/UI/movies_list_view.dart
+++ lib/app/modules/movies/UI/movies_list_view.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:biblioteca_flutter/app/modules/movies/controller/movies_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -8,43 +9,67 @@ class MoviesListView extends GetView<MoviesController> {
 
   @override
   Widget build(BuildContext context) {
-    final List<String> titles = [
-      "RED",
-      "",
-    ];
+    return Obx(() {
+      if (controller.isLoading.value) {
+        return Center(child: CircularProgressIndicator());
+      }
 
-    final List<Widget> images = [
-      Container(color: Colors.red),
-      Container(
-        color: Colors.transparent,
-        child: Center(
-          child: Container(
-            width: 72,
-            height: 72,
-            decoration: BoxDecoration(color: Colors.black),
-            child: const Icon(Icons.add, size: 40, color: Colors.white),
+      final titles = [
+        ...controller.movies.map((movie) => movie.title),
+        ""
+      ];
+
+      final images = [
+        ...controller.movies.map((movie) {
+          DecorationImage? decorationImage;
+          if (movie.imageBase64 != null && movie.imageBase64!.isNotEmpty) {
+            final bytes = base64Decode(movie.imageBase64!);
+            decorationImage = DecorationImage(
+              image: MemoryImage(bytes),
+              fit: BoxFit.cover,
+            );
+          }
+
+          return Container(
+            decoration: BoxDecoration(
+              image: decorationImage,
+              color: Colors.blue,
+            ),
+            child: Container(
+              color: Colors.black.withValues(alpha: 0.3),
+            ),
+          );
+        }),
+        Container(
+          color: Colors.transparent,
+          child: Center(
+            child: Container(
+              width: 72,
+              height: 72,
+              decoration: BoxDecoration(color: Colors.black),
+              child: const Icon(Icons.add, size: 40, color: Colors.white),
+            ),
           ),
         ),
-      ),
-    ];
-    return VerticalCardPager(
-      titles: titles, // required
-      images: images, // required
-      textStyle: TextStyle(
-        color: Colors.white,
-        fontWeight: FontWeight.bold,
-      ), // optional
-      onPageChanged: (page) {
-        // optional
-      },
-      onSelectedItem: (index) {
-        if (index == images.length - 1) {
-          controller.addNewMovie();
-        }
-      },
-      initialPage: 0, // optional
-      align: ALIGN.CENTER, // optional
-      physics: ClampingScrollPhysics(), // optional
-    );
+      ];
+
+      return VerticalCardPager(
+        titles: titles,
+        images: images,
+        textStyle: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        onPageChanged: (page) {},
+        onSelectedItem: (index) {
+          if (index == images.length - 1) {
+            controller.addNewMovie();
+          }
+        },
+        initialPage: 0,
+        align: ALIGN.CENTER,
+        physics: ClampingScrollPhysics(),
+      );
+    });
   }
 }
