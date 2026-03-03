a6e88a6c36b7d93c901293b2dc88c8ec23d63290
Davi Almeida Matta da Silva <davialmeidamatta@gmail.com>
2026-03-03 13:20:51 -0300

    continuação do  feat anterior

---
+++ lib/app/modules/login/service/sign_in_service.dart
@@ -0,0 +1,54 @@
+import 'package:biblioteca_flutter/app/modules/login/data/model/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart' as fb;
+import 'package:google_sign_in/google_sign_in.dart';
+
+
+class SignInService {
+  final auth = fb.FirebaseAuth.instance;
+  
+  final g_auth.GoogleSignIn googleSignIn = g_auth.GoogleSignIn();
+
+
+  SignInService();
+
+  Future<User?> signInWithGoogle() async {
+    try {
+      final googleUser = await googleSignIn.signIn();
+      return googleUser != null ? await _signIn(googleUser) : null;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future<User?> _signIn(GoogleSignInAccount googleUser) async {
+    try {
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      final credential = fb.GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      await auth.signInWithCredential(credential);
+      return _getFirebaseAuthUser();
+    } catch (e) {
+      return null;
+    }
+  }
+
+  User? _getFirebaseAuthUser() {
+    fb.User? user = auth.currentUser;
+    if (user == null) return null;
+
+    return User(
+      id: user.uid,
+      name: user.displayName ?? "",
+      email: user.email ?? "",
+    );
+  }
+}
+
+extension on GoogleSignInAuthentication {
+  String? get accessToken => null;
+}
